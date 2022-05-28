4f2ab21a96d799bc4bbc269ee50be4475d4bdab7
tecrodrigocastro <tec.rodrigocastro@gmail.com>
2022-05-28 15:16:08 -0300

    Consumo de API com Http

--- lib/cases/apis/playground.dart
+++ lib/cases/apis/playground.dart
@@ -1,7 +1,10 @@
 import 'dart:io';
 
-void main() {
+void main() async {
   var url = 'https://jsonplaceholder.typicode.com/todos';
 
   final client = HttpClient();
+  //request
+  final request = await client.openUrl('get', Uri.parse(url));
+  //response
 }
