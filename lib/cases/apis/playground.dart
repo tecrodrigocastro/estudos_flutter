0323a0b7f355d100cb9bbbf7033fb2fa3485fc4a
tecrodrigocastro <tec.rodrigocastro@gmail.com>
2022-05-28 15:58:17 -0300

    Consumo de Api - request response

--- lib/cases/apis/playground.dart
+++ lib/cases/apis/playground.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 
 void main() async {
@@ -6,5 +7,15 @@ void main() async {
   final client = HttpClient();
   //request
   final request = await client.openUrl('get', Uri.parse(url));
+  request.headers.set('content-type', 'application/json');
   //response
+  final response = await request.close();
+  final list = <String>[];
+
+  await for (var data in response) {
+    final string = utf8.decode(data);
+    list.add(string);
+  }
+  final body = list.join();
+  print(body);
 }
